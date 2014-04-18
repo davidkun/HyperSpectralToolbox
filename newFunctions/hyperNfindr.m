5c53fe616d8efcd9be85e8c09ae4d14420f13a29
David Kun <davidkun4@gmail.com>
2014-04-18 17:37:42 -0400

    finished NFINDR algorithm

--- newFunctions/hyperNfindr.m
+++ newFunctions/hyperNfindr.m
@@ -1,22 +1,80 @@
-function [ U, X, n ] = hyperNfindr( M, q, U_init )
+function [U] = hyperNfindr(M, q)
 % HYPERNFINDR Performs the N-FINDR (endmember extraction) algorithm
-%   Performs the N-FINDR algorithm to generate abundance maps
-% and find the purest pixels.  This function utilizes FastICA.
+%   Performs the N-FINDR algorithm to find the purest pixels
+% and generate abundance maps.
 %
 % Usage
-%   [ U, X, n ] = hyperNfindr( M, q, U_init )
+%   [U] = hyperNfindr(M)
+%   [U] = hyperNfindr(M, q)
 % Inputs
-%   M - HSI data in 2D (p x N)
-%   q - Number of materials to unmix
-%   U_init - Initia l endmembers (p x #)
+%   M - 2d matrix of HSI data (p x N)
+%   q - Number of endmembers to find (if not given, default q equals p-1)
 % Outputs
-%   U - matrix of recovered endmembers (p x q)
-%   X - material abundance maps (q x N)
-%   n - (optional) Indicies of recovered endmembers (q x 1)
+%   U - Recovered endmembers (p x N)
 %
+% If only M is given as input, this function calls hyperHfcVd to estimate
+%  the number of endmembers (q) and then PCA to reduce dimensionality to (q-1).
+% 
 % References
 %   M. Winter, "N-findr: an algorithm for fast autonomous 
 % spectral endmember determination in hyperspectral data," SPIE’s 
 % International Symposium on Optical Science, Engineering, and 
 % Instrumentation, pages 266–275. International Society for Optics 
 % and Photonics, 1999.
+
+[p, N] = size(M);
+M_orig = M;
+if nargin == 1
+    q = hyperHfcVd(M_orig, [10^-3]);
+    M = hyperPct(M, q-1);
+elseif q < p+1
+    M = hyperPct(M, q-1);
+    warning('WarnTests:dim', ...
+    strcat('N-FINDR requires (q+1) spectral bands.',...
+           '\nPerforming PCA to reduce dimensionality.'))
+elseif q > p+1
+    warning('WarnTests:dim', ...
+    strcat('N-FINDR requires (q+1) spectral bands.',...
+           '\nPerforming PCA to reduce dimensionality.'))
+    error('ErrTests:dim', ...
+        strcat('N-FINDR cannot find more than (p+1) endmembers (q),\n', ...
+               'where p is the number of available spectral bands.'))
+end
+
+% Initialize
+M         = M*1e4;
+U_idx     = randperm(N,q);
+E         = M(:,U_idx);
+V         = abs(det([ones(1,q); E])) / factorial(q-1);
+vols      = zeros(q,1);
+
+% Search for maximum volume
+for j = 1:N;
+    % Replace each column of E with sample vector M(:,j) 
+    % and compute the volume for each
+    for k = 1:q;
+        E_tmp      = E;
+        E_tmp(:,k) = M(:,j);
+        vols(k)    = abs(det([ones(1,q); E_tmp])) / factorial(q-1);
+    end
+    % If max volume is greater than previous V, update E and V
+    [V_tmp,k_idx] = max(vols);
+    if V_tmp > V
+        V            = V_tmp;
+        E(:,k_idx)   = M(:,j);
+        U_idx(k_idx) = j;
+    end
+end
+
+% Return endmembers
+U = M_orig(:, U_idx);
+
+
+
+
+
+
+
+
+
+
