e0c1404a82b916ac40f9a1b6d44dc21deec7b71b
David Kun <davidkun4@gmail.com>
2014-04-30 10:34:14 -0400

    organized code, removed unnecessary code, improved figure saving

--- newFunctions/hyperDemo2.m
+++ newFunctions/hyperDemo2.m
@@ -1,34 +1,53 @@
 function hyperDemo2
 % HYPERDEMO2 Demonstrates new functions in the hyperspectral toolbox
-clear all; close all; clc; dbstop if error;
+clear all; close all; clc;
 %--------------------------------------------------------------------------
 % Measurements and reflectance (input) files/directory
-% dataDir    = ['~' filesep 'Downloads' filesep 'data'];
-% rflFile    = [dataDir filesep 'f970620t01p02_r03_sc02.a.rfl']
-% spcFile = [dataDir filesep 'f970620t01p02_r03.a.spc'];
+% The data files are obtained from: http://aviris.jpl.nasa.gov/data/free_data.html
+% (the reflectance .rar file for Cuprite)
 dataDir    = ['~' filesep 'Downloads' filesep 'f970619t01p02r02c'];
-rflFile    = [dataDir filesep 'f970619t01p02_r02_sc01.a.rfl'];
+rflFile    = [dataDir filesep 'f970619t01p02_r02_sc04.a.rfl'];
 spcFile = [dataDir filesep 'f970619t01p02_r02.a.spc'];
 
 % Results (output) directory
-resultsDir = ['~' filesep 'Downloads' filesep 'data' filesep 'results'];
+resultsDir = '~/Dropbox/Purdue Grad/Semester 2/AAE 590/Project/Simulation/results';
 %--------------------------------------------------------------------------
 
-fprintf('  Reading data from %s \n  in the directory %s.\n', rflFile, resultsDir);
+fprintf('  Reading data from %s \n  in the directory %s.\n', rflFile, dataDir);
 fprintf('  Storing results in %s directory.\n', resultsDir);
 if ~isdir(resultsDir)
     mkdir(resultsDir);
 end
 
-% Read in an HSI image and display one band
-bndnum  = 132; % Band Number
-slice   = hyperReadAvirisRfl(rflFile, [400 500], [1 614], [bndnum bndnum]);
+%% Read in an HSI image and display one band
+bndnum  = 120; % Band Number
+slice   = hyperReadAvirisRfl(rflFile, [1 512], [1 614], [bndnum bndnum]);
 figure; imagesc(slice); axis image; colormap(gray);
-title(strcat('Band ', num2str(bndnum)));
+title(sprintf('Hyperspectral Image, band = %d',bndnum));
+
+%% Pseudo-True Color Image, rgb = [32,19,10]
+% rmap = hyperReadAvirisRfl(rflFile, [1 512], [1 614], [32 32]);
+% gmap = hyperReadAvirisRfl(rflFile, [1 512], [1 614], [19 19]);
+% bmap = hyperReadAvirisRfl(rflFile, [1 512], [1 614], [10 10]);
+% 
+% figure;
+% hRmap = imagesc(rmap); axis image; hold on;
+% colormap([linspace(0,1,10)',zeros(10,1),zeros(10,1)]);
+% alpha('color'); 
+% rAlpha = get(hRmap, 'AlphaData');
+% hGmap = imagesc(gmap); axis image;
+% colormap([zeros(10,1),linspace(0,1,10)',zeros(10,1)]);
+% alpha('color'); 
+% gAlpha = get(hGmap, 'AlphaData');
+% hBmap = imagesc(bmap);
+% colormap([zeros(10,1),zeros(10,1),linspace(0,1,10)']);
+% alpha('color'); 
+% set(hRmap, 'AlphaData', rAlpha);
+% set(hGmap, 'AlphaData', gAlpha);
 
 %% Read part of AVIRIS reflectance data file that we will further process
-M = hyperReadAvirisRfl(rflFile, [400 500], [1 614], [1 224]);
-M = hyperNormalize(M);
+M = hyperReadAvirisRfl(rflFile, [1 512], [1 614], [1 224]);
+% M = hyperNormalize(M);
 
 % Read AVIRIS .spc file
 lambdasNm = hyperReadAvirisSpc(spcFile);
@@ -37,93 +56,120 @@ lambdasNm = hyperReadAvirisSpc(spcFile);
 [h, w, p] = size(M);
 M = hyperConvert2d(M);
 desiredLambdasNm = 400:(2400-400)/(224-1):2400;
+
+fprintf('Resampling M for desired wavelengths...\n'); tic;
 M = hyperResample(M, lambdasNm, desiredLambdasNm);
+et=toc; fprintf('...Done. (~%1.3g seconds)\n',et);
 
-% Remove low SNR bands.
-goodBands = [10:100 116:150 180:216];
-M = M(goodBands, :);
-p = length(goodBands);
+% Remove low SNR bands and bands affected by water absorption
+%  ( source: Chang, Chein-I., "Random N-finder (N-FINDR) endmember 
+%  extraction algorithms for hyperspectral imagery." Image Processing,
+%  IEEE Transactions on 20.3 (2011): 641-656. )
+fprintf('Removing low SNR bands...\n');
+goodBands = [4:104 116:149 171:224];
+M         = M(goodBands, :);
+p         = length(goodBands);
+lambdasNm = desiredLambdasNm(goodBands);
 
+% Final hyperspectral data cube that will be used throughout this demo
 M = hyperConvert3d(M, h, w, p);
+fprintf('...Done\n');
 
 %% --------------------------------------------------------------------------
 % Perform various endmember determination algorithms
-q = 10; % number of endmembers
-% PPI
-Uppi = hyperPpi(hyperConvert2d(M), q, 1000);
-figure; plot(desiredLambdasNm(goodBands), Uppi); 
-title('PPI Recovered Endmembers'); grid on;
-ylabel('Reflectance [0-1]'); xlabel('Wavelength [nm]');
+q = 5;                      % number of endmembers to find
+Mnorm = hyperNormalize(M);  % normalized [0-1] reflectance
+
+%% PPI
+fprintf('Performing PPI for endmember determination...\n'); tic;
+Uppi = hyperPpi(hyperConvert2d(Mnorm), q, 1000);
+et=toc; fprintf('...Done. (~%1.3g seconds)\n',et);
+% Uppi = hyperNormalize(Uppi); % Normalize signatures
+
+% Plot endmember signatures
+figure; plot(lambdasNm, Uppi, '.'); grid on;
+title('PPI Recovered Endmembers', 'Interpreter', 'Latex', 'FontSize', 14);
+ax(1) = ylabel('Reflectance [0-1]'); 
+ax(2) = xlabel('Wavelength [nm]');
+set(ax, 'Interpreter', 'Latex', 'FontSize', 12);
+l = legend(cellstr(num2str((1:q)'))', 'Location', 'EastOutside');
+a = get(l, 'children'); set(a(1:3:end), 'MarkerSize', 20);
+print(gcf, '-r600', '-depsc', sprintf('%s/endmmbrs-ppi', resultsDir));
 
 %% N-FINDR
-Unfindr = hyperNfindr(hyperConvert2d(M), q);
-figure; plot(desiredLambdasNm(goodBands), Unfindr); 
-title('N-FINDR Recovered Endmembers'); grid on;
-ylabel('Reflectance [0-1]'); xlabel('Wavelength [nm]');
+fprintf('Performing N-FINDR for endmember determination...\n'); tic;
+Unfindr = hyperNfindr(hyperConvert2d(Mnorm), q);
+et=toc; fprintf('...Done. (~%1.3g seconds)\n',et);
+
+% Plot endmember signatures
+figure; plot(lambdasNm, Unfindr, '.'); grid on;
+title('N-FINDR Recovered Endmembers', 'Interpreter', 'Latex', 'FontSize', 14);
+ax(1) = ylabel('Reflectance [0-1]'); 
+ax(2) = xlabel('Wavelength [nm]');
+set(ax, 'Interpreter', 'Latex', 'FontSize', 12);
+l = legend(cellstr(num2str((1:q)'))', 'Location', 'EastOutside');
+a = get(l, 'children'); set(a(1:3:end), 'MarkerSize', 20);
+print(gcf, '-r600', '-depsc', sprintf('%s/endmmbrs-nfindr', resultsDir));
 
 %% AVMAX
-Uavmax = hyperAvmax(hyperConvert2d(M), q);
-figure; plot(desiredLambdasNm(goodBands), Uavmax); 
-title('AVMAX Recovered Endmembers'); grid on;
-ylabel('Reflectance [0-1]'); xlabel('Wavelength [nm]');
+fprintf('Performing AVMAX for endmember determination...\n'); tic;
+Uavmax = hyperAvmax(hyperConvert2d(Mnorm), q);
+et=toc; fprintf('...Done. (~%1.3g seconds)\n',et);
+
+% Plot endmember signatures
+figure; plot(lambdasNm, Uavmax, '.'); grid on;
+title('AVMAX Recovered Endmembers', 'Interpreter', 'Latex', 'FontSize', 14);
+ax(1) = ylabel('Reflectance [0-1]'); 
+ax(2) = xlabel('Wavelength [nm]');
+set(ax, 'Interpreter', 'Latex', 'FontSize', 12);
+l = legend(cellstr(num2str((1:q)'))', 'Location', 'EastOutside');
+a = get(l, 'children'); set(a(1:3:end), 'MarkerSize', 20);
+print(gcf, '-r600', '-depsc', sprintf('%s/endmmbrs-avmax', resultsDir));
 
 %% --------------------------------------------------------------------------
-% Perform a fully unsupervised exploitation chain using HFC, ATGP, and NNLS
-fprintf('Performing fully unsupervised exploitation using HFC, ATGP, and NNLS...\n');
-M = hyperConvert2d(M);
-
-% Estimate number of endmembers in image.
-q = hyperHfcVd(M, [10^-3]);
-% q = 50;
-
-% PCA the data to remove noise
-% hyperWhiten(M)
-M = hyperPct(M, q);
-
-% Unmix AVIRIS image.
-% U = hyperVca(M, q);
-Uatgp = hyperAtgp(M, q);
-figure; plot(desiredLambdasNm(goodBands), Uatgp); 
-title('ATGP Recovered Endmembers'); grid on;
-ylabel('Reflectance [0-1]'); xlabel('Wavelength [nm]')
-
-%% Create abundance maps from unmixed endmembers
-% From ATGP results:
-abundanceMaps = hyperNnls(M, Uatgp);
-abundanceMaps = hyperConvert3d(abundanceMaps, h, w, q);
-
-for i=1:q
-    tmp = hyperOrthorectify(abundanceMaps(:,:,i), 21399.6, 0.53418);
-    figure; imagesc(tmp); colorbar; axis image; 
-        title(sprintf('Abundance Map %d', i));
-        hyperSaveFigure(gcf, sprintf('%s/chain-atgp-%d.png', resultsDir, i));
-        close(gcf);
-end
-fprintf('Done.\n');
-
-% From PPI results:
+% Create abundance maps from unmixed endmembers
+%% From PPI results:
+fprintf('Creating abundance maps from PPI endmember results...\n'); tic;
 abundanceMaps = hyperNnls(hyperConvert2d(M), Uppi);
 abundanceMaps = hyperConvert3d(abundanceMaps, h, w, q);
-
+et=toc; fprintf('...Done. (~%1.3g seconds)\n',et);
+%%
+fprintf('Plotting and saving PPI abundance maps...\n');
 for i=1:q
-    tmp = hyperOrthorectify(abundanceMaps(:,:,i), 21399.6, 0.53418);
-    figure; imagesc(tmp); colorbar; axis image; 
-        title(sprintf('Abundance Map %d', i));
-        hyperSaveFigure(gcf, sprintf('%s/chain-ppi-%d.png', resultsDir, i));
-        close(gcf);
+    if i==1; figure; end;
+    clf; imagesc(abundanceMaps(:,:,i)); colorbar; axis image; 
+    title(sprintf('Abundance Map %d/%d', i, q), 'Interpreter', 'Latex');
+    print(gcf, '-depsc', '-r600', sprintf('%s/abund-ppi-%d', resultsDir, i))
 end
-fprintf('Done.\n');
+close(gcf); fprintf('...Done.\n');
 
-% From N-FINDR results:
+%% From N-FINDR results:
+fprintf('Creating abundance maps from N-FINDR endmember results...\n'); tic;
 abundanceMaps = hyperNnls(hyperConvert2d(M), Unfindr);
 abundanceMaps = hyperConvert3d(abundanceMaps, h, w, q);
+et=toc; fprintf('...Done. (~%1.3g seconds)\n',et);
 
+fprintf('Plotting and saving N-FINDR abundance maps...\n');
 for i=1:q
-    tmp = hyperOrthorectify(abundanceMaps(:,:,i), 21399.6, 0.53418);
-    figure; imagesc(tmp); colorbar; axis image; 
-        title(sprintf('Abundance Map %d', i));
-        hyperSaveFigure(gcf, sprintf('%s/chain-nfindr-%d.png', resultsDir, i));
-        close(gcf);
+    if i==1; figure; end;
+    clf; imagesc(abundanceMaps(:,:,i)); colorbar; axis image; 
+    title(sprintf('Abundance Map %d/%d', i, q), 'Interpreter', 'Latex');
+    print(gcf, '-depsc', '-r600', sprintf('%s/abund-nfindr-%d', resultsDir, i))
 end
-fprintf('Done.\n');
+close(gcf); fprintf('...Done.\n');
+
+%% From AVMAX results:
+fprintf('Creating abundance maps from AVMAX endmember results...\n'); tic;
+abundanceMaps = hyperNnls(hyperConvert2d(M), Uavmax);
+abundanceMaps = hyperConvert3d(abundanceMaps, h, w, q);
+et=toc; fprintf('...Done. (~%1.3g seconds)\n',et);
+
+fprintf('Plotting and saving AVMAX abundance maps...\n');
+for i=1:q
+    if i==1; figure; end;
+    clf; imagesc(abundanceMaps(:,:,i)); colorbar; axis image; 
+    title(sprintf('Abundance Map %d/%d', i, q), 'Interpreter', 'Latex');
+    print(gcf, '-depsc', '-r600', sprintf('%s/abund-avmax-%d', resultsDir, i))
+end
+close(gcf); fprintf('...Done.\n');
 
